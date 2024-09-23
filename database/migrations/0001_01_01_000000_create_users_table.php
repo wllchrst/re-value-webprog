346e7108e92397de46fa0d846f1360a6763e34f8
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-09-23 16:05:49 +0700

    feat: Product's attribute definition

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,11 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id("id");
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->longText("description");
+            $table->string("image_path")->default("");
             $table->string('password');
+            $table->integer("point")->default(0);
             $table->rememberToken();
             $table->timestamps();
         });

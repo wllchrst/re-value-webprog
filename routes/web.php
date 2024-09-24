085036db2355b5435f5e4e814cfb96d666518c36
William Christian <william.christian001@binus.ac.id>
2024-09-24 21:14:17 +0700

    feat: Add page and routing

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::get("", [AuthController::class, "showLoginForm"]);
+Route::get("/home", [HomeController::class, "getHomePage"])->name("home");
 
 Route::prefix("user")->group(function() {
     // get pages

4e3be5e7f6dbfe31180831635c4942a68921f927
William Christian <william.christian001@binus.ac.id>
2024-09-24 20:27:48 +0700

    feat: add routing and with names

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::get("/", [AuthController::class, "showLoginForm"]);
+Route::get("", [AuthController::class, "showLoginForm"]);
 
 Route::prefix("user")->group(function() {
+    // get pages
+    Route::get("/register", [AuthController::class, "showRegisterForm"])->name("user.register");
+
     Route::post("/login", [AuthController::class, "login"])->name("user.login");
+    Route::post("/register", [AuthController::class,"register"])->name("user.register");
 });

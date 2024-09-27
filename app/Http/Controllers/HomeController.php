720fc9ea71ea9a0ee9c9f8a8a4e34a26b544ef69
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-09-27 13:14:59 +0700

    feat: show all product that are being sold in the home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function getHomePage(){
-        return view("pages.home");
+    public function getHomePage()
+    {
+        $products = Product::all();
+        return view("pages.home", compact("products"));
     }
 }

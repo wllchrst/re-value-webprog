23eebc6e595e0acfad105cb92aeaf74ebc219d38
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-09-27 13:15:16 +0700

    feat: controller for new model User Products

---
+++ app/Http/Controllers/UserProductsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserProducts;
+use Illuminate\Http\Request;
+
+class UserProductsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UserProducts $userProducts)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UserProducts $userProducts)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, UserProducts $userProducts)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UserProducts $userProducts)
+    {
+        //
+    }
+}

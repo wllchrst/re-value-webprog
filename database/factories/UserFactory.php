23f932752c019699b58555fd95b157169f149563
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-09-27 13:17:00 +0700

    feat: user factory for creating new user using faker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,13 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'name' => $this->faker->name(),                             // Random name
+            'email' => $this->faker->unique()->safeEmail(),            // Unique email address
+            'description' => $this->faker->paragraph(),                 // Random description
+            'image_path' => $this->faker->imageUrl(640, 480, 'people'), // Random image URL
+            'password' => bcrypt('password'),                             // Default password
+            'point' => $this->faker->numberBetween(0, 100),             // Random point value
+            'remember_token' => Str::random(10),                        // Random remember token
         ];
     }
 
@@ -37,7 +39,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }

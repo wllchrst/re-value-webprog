8cc61f2995c623d601239899b944b02643ce7de1
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2024-09-30 12:05:44 +0700

    feat: add routing

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 
 Route::get("", [AuthController::class, "showLoginForm"]);
 Route::get("/home", [HomeController::class, "getHomePage"])->name("home");
+Route::get("/profile", [ProfileController::class, 'showProfilePage'])->name("profile");
 
-Route::prefix("user")->group(function() {
+Route::prefix("user")->group(function () {
     // get pages
     Route::get("/register", [AuthController::class, "showRegisterForm"])->name("user.register");
 
     Route::post("/login", [AuthController::class, "login"])->name("user.login");
-    Route::post("/register", [AuthController::class,"register"])->name("user.register");
+    Route::post("/register", [AuthController::class, "register"])->name("user.register");
     Route::post("/logout", [AuthController::class, "logout"])->name("user.logout");
+    Route::post("/update", [ProfileController::class, "updateUser"])->name("user.update");
 });

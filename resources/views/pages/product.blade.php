56e911019559ed7c250c159a9a32a62a5071cfc1
William Christian <william.christian001@binus.ac.id>
2024-12-02 21:49:13 +0700

    feat: empty page for products and items

---
+++ resources/views/pages/product.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main-page-layout')
+
+@section('title', 'Home')
+
+@section('page-title', 'Home Page')
+
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+@endsection
+
+@section('content')
+
+@endsection

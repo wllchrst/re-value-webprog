1083e435572cbca9575af6e15e1f69c10581b580
William Christian <william.christian001@binus.ac.id>
2024-12-02 21:48:37 +0700

    feat: add item type for profile page

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ItemType;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,7 +11,8 @@ class ProfileController extends Controller
     public function showProfilePage()
     {
         $user = Auth::user();
-        return view("pages.profile", compact('user'));
+        $item_types = ItemType::all();
+        return view("pages.profile", compact('user', 'item_types'));
     }
 
     public function updateUser(Request $request)

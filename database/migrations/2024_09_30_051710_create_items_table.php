7d886edde6da610ae61d3e378407ac4f8ef87def
William Christian <william.christian001@binus.ac.id>
2024-12-02 21:46:18 +0700

    feat: migration add item types and weight for item

--- database/migrations/2024_09_30_051710_create_items_table.php
+++ database/migrations/2024_09_30_051710_create_items_table.php
@@ -20,7 +20,9 @@ return new class extends Migration
                 $table->string("image_path");
                 $table->boolean("sold");
                 $table->unsignedBigInteger("user_id");
+                $table->unsignedBigInteger("item_type_id");
                 $table->foreign("user_id")->references("id")->on("users")->onDelete('cascade');
+                $table->foreign("item_type_id")->references("id")->on("item_types")->onDelete('cascade');
                 $table->timestamps();
             }
         );

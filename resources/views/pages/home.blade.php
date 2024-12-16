c67ddf147e1b0d3235a0b70fc29b85a39fae9e52
adilanshr <nasha.yuhanas@gmail.com>
2024-12-16 18:27:28 +0700

    edit mainpoint home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -44,20 +44,20 @@
     <div class="jargon animate__animated animate__fadeInUp">
         <p>Together, we’re crafting a cleaner, brighter future—one transaction at a time. 🌍✨</p>
     </div>
-    <div class="container mt-5">
+    <div class="container-mainpoint mt-5">
         <h2 class="text-center mb-4 animate__animated animate__fadeInDown animate__delay-1s animate__duration-2s" style="color: #2C6B49">Re-Value can do it all</h2>
         <div class="row text-center">
-            <div class="col-md-6 mb-4 animate__animated animate__fadeInLeft animate__delay-1s animate__duration-2s">
+            <div class="col-md-4 mb-4 animate__animated animate__fadeInLeft animate__delay-1s animate__duration-2s">
                 <img src="/website-content/Recycle_icon.png" alt="Recycled Products" class="img-fluid mb-3" style="width: 100px;">
                 <h5>Recycled Products</h5>
                 <p>Buy eco-friendly items made from recycled materials</p>
             </div>
-            <div class="col-md-6 mb-4 animate__animated animate__fadeInRight animate__delay-1s animate__duration-2s">
+            <div class="col-md-4 mb-4 animate__animated animate__fadeInRight animate__delay-1s animate__duration-2s">
                 <img src="/website-content/SaleTag_icon.png" alt="Sell Used Items" class="img-fluid mb-3" style="width: 100px;">
                 <h5>Sell Used Items</h5>
                 <p>Sell used bottles, cardboard, cans, and earn points</p>
             </div>
-            <div class="col-md-12 mb-4 animate__animated animate__fadeInUp animate__delay-1s animate__duration-2s">
+            <div class="col-md-4 mb-4 animate__animated animate__fadeInUp animate__delay-1s animate__duration-2s">
                 <img src="/website-content/Coin_icon.png" alt="Earn Points for Recycling" class="img-fluid mb-3" style="width: 100px;">
                 <h5>Earn Points for Recycling</h5>
                 <p>Get points for submitting used items. Redeem for rewards!</p>

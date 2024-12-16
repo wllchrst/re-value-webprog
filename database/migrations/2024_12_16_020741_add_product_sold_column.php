df6c344c6fd6ba6c5693ee3e308cfa664f8ab7f9
Yossyfrenlie <yossyfrenlielo@gmail.com>
2024-12-16 15:09:31 +0700

    add default value for sold in migrations

--- database/migrations/2024_12_16_020741_add_product_sold_column.php
+++ database/migrations/2024_12_16_020741_add_product_sold_column.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         //
         Schema::table('products', function (Blueprint $table) {
-            $table->boolean("sold");
+            $table->boolean("sold")->default(false);
         });
     }
 
